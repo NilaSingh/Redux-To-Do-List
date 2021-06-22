36a1e71d7be8674107b4f356b2fbe27ba584ed85
Nila <Nila@LAPTOP-M0QERO78>
2021-06-22 06:56:26 -0400

    to do list complete

---
+++ todolist_app/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+const Todos = () => {
+  const dispatch = useDispatch()
+  const todos = useSelector(state => state.todos)
+  const handleClick = id => dispatch({
+    type: 'DELETE_TODO',
+    payload:id,
+  })
+  if(!todos || ! todos.length){
+    return(
+      <p>No to dos</p>
+    )
+  }
+  return (
+    <ul>
+      {todos.map(todo => <li onClick={() => handleClick(todo.id)}>{todo.label}</li>)}
+    </ul>
+  )
+}
+const TodoInput = () => {
+  const dispatch = useDispatch()
+  const [newTodo, setNewTodo]=useState()
+  const handleChange = event => setNewTodo(event.target.value)
+  const handleClick = () => dispatch({
+    type:'ADD_TODO',
+    payload:{
+      label: newTodo,
+      id:Math.ceil(Math.random() * 100)
+    }
+  })
+  return (
+    <>
+    <input value={newTodo} onChange={handleChange} type='text' />
+    <button onClick={handleClick}> Add To Do </button>
+    </>
+  )
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          To Do List:
+        </p>
+        <Todos />
+          <TodoInput />
+      </header>
+    </div>
+  );
+}
+
+export default App;
